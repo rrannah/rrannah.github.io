dc786b23a03cd8271c1b88aeb82be0fc729524d5
rrannah <106163823+rrannah@users.noreply.github.com>
2023-10-11 10:26:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,194 @@
+// Targeting video element
+let clip = document.querySelector(".vid1");
+
+let clip2 = document.querySelector(".vid2");
+let clip3 = document.querySelector(".vid3");
+let clip5 = document.querySelector(".vid5");
+let clip6 = document.querySelector(".vid6");
+let clip7 = document.querySelector(".vid7");
+let clip8 = document.querySelector(".vid8");
+let clip10 = document.querySelector(".vid10");
+let clip11 = document.querySelector(".vid11");
+let clip12 = document.querySelector(".vid12");
+let clip13= document.querySelector(".vid13");
+let clip14= document.querySelector(".vid14");
+let clip16= document.querySelector(".vid16");
+let clip17= document.querySelector(".vid17");
+let clip18= document.querySelector(".vid18");
+
+
+clip.addEventListener("mouseover", function (e) {
+  
+    clip.play();
+
+});
+
+
+clip.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip.pause();}, 5000);  
+  
+});
+
+clip2.addEventListener("mouseover", function (e) {
+    clip2.play();
+});
+
+clip2.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip2.pause();}, 5000);  
+});
+
+clip3.addEventListener("mouseover", function (e) {
+    clip3.play();
+});
+
+clip3.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip3.pause();}, 5000);  
+});
+
+// clip4.addEventListener("mouseover", function (e) {
+//     clip4.play();
+// });
+
+// clip4.addEventListener("mouseout", function (e) {
+//     setTimeout(()=>{  clip4.pause();}, 5000);  
+// });
+
+clip5.addEventListener("mouseover", function (e) {
+    clip5.play();
+});
+
+clip5.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip5.pause();}, 5000);  
+});
+
+
+clip6.addEventListener("mouseover", function (e) {
+    clip6.play();
+});
+
+clip6.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip6.pause();}, 5000);  
+});
+
+clip7.addEventListener("mouseover", function (e) {
+    clip7.play();
+});
+
+
+clip7.addEventListener("mouseover", function (e) {
+    setTimeout(()=>{  clip7.pause();}, 5000);  
+});
+
+clip8.addEventListener("mouseout", function (e) {
+
+    clip8.play();
+});
+
+
+clip8.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip8.pause();}, 5000);  
+});
+
+// clip9.addEventListener("mouseout", function (e) {
+
+//     clip9.play();
+// });
+
+
+// clip9.addEventListener("mouseout", function (e) {
+//     setTimeout(()=>{  clip9.pause();}, 5000);  
+// });
+
+clip10.addEventListener("mouseout", function (e) {
+ 
+    clip10.play();
+});
+
+
+clip10.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip10.pause();}, 5000);  
+});
+
+clip11.addEventListener("mouseout", function (e) {
+
+    clip11.play();
+});
+
+
+clip11.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip11.pause();}, 5000);  
+});
+
+clip12.addEventListener("mouseout", function (e) {
+
+    clip12.play();
+});
+
+
+clip12.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip12.pause();}, 5000);  
+});
+
+clip13.addEventListener("mouseout", function (e) {
+
+    clip13.play();
+});
+
+
+clip13.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip13.pause();}, 5000);  
+});
+
+
+clip14.addEventListener("mouseout", function (e) {
+
+    clip14.play();
+});
+
+
+clip14.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip14.pause();}, 5000);  
+});
+
+// clip15.addEventListener("mouseout", function (e) {
+
+//     clip15.play();
+// });
+
+
+// clip15.addEventListener("mouseout", function (e) {
+//     setTimeout(()=>{  clip15.pause();}, 5000);  
+// });
+
+clip16.addEventListener("mouseout", function (e) {
+   
+    clip16.play();
+});
+
+
+clip16.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip16.pause();}, 5000);  
+});
+
+
+
+clip17.addEventListener("mouseout", function (e) {
+
+    clip17.play();
+});
+
+
+clip17.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip17.pause();}, 5000);  
+});
+
+
+clip18.addEventListener("mouseout", function (e) {
+
+    clip18.play();
+});
+
+
+clip18.addEventListener("mouseout", function (e) {
+    setTimeout(()=>{  clip18.pause();}, 5000);  
+});
